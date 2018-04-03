7764a88b6bbaa63cfc001dcae17a67246c291e29
KYIV\Dmytro_Yerusalimtsev <dmytro_yerusalimtsev@epam.com>
2018-04-03 17:07:45 +0300

    Added cluster initialization logic.

--- car-service/application/src/main/scala/com/cartracker/application/Main.scala
+++ car-service/application/src/main/scala/com/cartracker/application/Main.scala
@@ -4,6 +4,6 @@ import akka.actor.ActorSystem
 import com.cartracker.application.actors.ClusterController
 
 object Main extends App {
-  val system = ActorSystem("Car Tracker Cluster")
+  val system = ActorSystem("CarTracker")
   val clusterController = system.actorOf(ClusterController.props(), "clusterController")
 }
